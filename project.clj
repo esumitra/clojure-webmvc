2376c20e98797bba02a6dd7fc6ee32450280c0b8
Edward Sumitra <esumitra@github>
2012-07-16 08:11:46 -0400

    added project file and empty directory structure

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject org.clojars.ed_sumitra/clojure-webmvc "1.0.0-SNAPSHOT"
+  :description "A clojure framework for MVC web applications using standard conventions"
+  :url "https://github.com/esumitra/clojure-webmvc"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/clojure_webmvc/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-webmvc.test.core
+  (:use [clojure-webmvc.core])
+  (:use [clojure.test]))
+
+(deftest placeholder
+  (is false "No tests have been written."))
