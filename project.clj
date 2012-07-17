a3fa2c213bef6bffd93ebbf278bee1f60c7d6e83
Edward Sumitra <esumitra@github>
2012-07-17 07:47:45 -0400

    added compojure dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject org.clojars.ed_sumitra/clojure-webmvc "1.0.0-SNAPSHOT"
   :description "A clojure framework for MVC web applications using standard conventions"
   :url "https://github.com/esumitra/clojure-webmvc"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [compojure "1.0.4"]])
